6d15f1ce9e069d560a3d39b45f3b7cc77cd3f53b
SimonAMott <simon_mott@mac.com>
2024-06-07 14:49:34 +0100

    2nd commit - initial file strucure

---
+++ src/test/java/com/autoventive/vehicles/VehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoventive.vehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
